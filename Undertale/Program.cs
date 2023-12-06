300ce488feb1e63631fa7a64c94df7665e2df2f1
Timothé Kobel <pa51skt@eduvaud.ch>
2023-12-06 09:02:46 +0100

    fix : Connection to Database (with admin Account) and run the game

--- Undertale/Program.cs
+++ Undertale/Program.cs
@@ -4,17 +4,6 @@ namespace Undertale
 {
     internal static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new Undertale());
-        }
         static List<string> customers = new List<string>();
 
         static void Main(string[] args)
@@ -25,6 +14,8 @@ namespace Undertale
             {
                 Console.WriteLine(customer);
             }
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Undertale());
         }
 
         static private List<string> ExecuteQuerySelect()
@@ -32,7 +23,7 @@ namespace Undertale
             List<string> queryResults = new List<string>();
 
             //TODO Improvement - use an external file to store sensitive data
-            string connString = "server=localhost;user=root;database=undertale;port=3306;password=Pa$$w0rd;";
+            string connString = "server=localhost;user=admin;database=undertale;port=3306;password=admin;";
 
             //prepare the connection
             MySqlConnection connection = new MySqlConnection(connString);
@@ -41,7 +32,7 @@ namespace Undertale
             //prepare the query
             //TODO Improvement - Using @parameter and value to build the query
             //TODO Improvement - https://learn.microsoft.com/en-us/dotnet/api/system.data.sqlclient.sqlparametercollection.addwithvalue?view=dotnet-plat-ext-7.0
-            string query = "SELECT player, game FROM undertale;";
+            string query = "SELECT * FROM player, game;";
 
             //set and execute the query 
             MySqlCommand cmd = new MySqlCommand(query, connection);
