af96773cf619407c51f15b48d30788242b3f7b53
Amin <amin@eduvaud.ch>
2023-12-12 10:10:39 +0100

    feat: game in mvp

--- Undertale/Program.cs
+++ Undertale/Program.cs
@@ -1,3 +1,4 @@
+using Org.BouncyCastle.Asn1.Crmf;
 using System;
 using System.Windows.Forms;
 using WindowsFormsApp1; // Assurez-vous d'ajuster le chemin vers votre espace de noms WindowsFormsApp1
@@ -12,7 +13,6 @@ namespace Undertale
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Undertale undertaleForm = new Undertale();
-
             undertaleForm.PlayButton.Click += (sender, e) => PlayButton_Click(undertaleForm);
             undertaleForm.QuitButton.Click += (sender, e) => QuitButton_Click(undertaleForm);
             Application.Run(undertaleForm);
@@ -22,21 +22,26 @@ namespace Undertale
         {
             string enteredName = gameForm.TextBox_EnterName.Text.Trim();
             Player.SendNickname(enteredName);
-            gameForm.ShowChoiceDungeon();
+            gameForm.EnterName_next_button.Visible = false;
+            gameForm.EnterName_pictureBox.Visible = false;
+            gameForm.TextBox_EnterName.Visible = false;
+            ShowIntro();
         }
 
         private static void PlayButton_Click(Undertale undertaleForm)
         {
-
             Game gameForm = new Game();
-
             gameForm.EnterName_next_button.Click += (sender, e) => EnterName_next_button_Click(gameForm);
-
             gameForm.Show();
         }
         private static void QuitButton_Click(Undertale undertaleForm)
         {
             Application.Exit();
+        }
+        public static void ShowIntro()
+        {
+            Intro intro = new Intro();
+            intro.Show();
         }
     }
 }
