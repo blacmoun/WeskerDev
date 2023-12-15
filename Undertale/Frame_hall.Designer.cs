95448775c471176f6248f367cb803fd08d66213b
Marc-Antoine Aboud <pl23idy@eduvaud.ch>
2023-12-15 09:35:07 +0100

    feat: Add frame Living room + Bathroom

--- Undertale/Frame_hall.Designer.cs
+++ Undertale/Frame_hall.Designer.cs
@@ -29,7 +29,49 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frame_hall));
+            Button_livingRoom = new WindowsFormsApp1.RJButton();
+            Button_Bathroom = new WindowsFormsApp1.RJButton();
             SuspendLayout();
+            // 
+            // Button_livingRoom
+            // 
+            Button_livingRoom.BackColor = Color.Transparent;
+            Button_livingRoom.BackgroundColor = Color.Transparent;
+            Button_livingRoom.BorderColor = Color.PaleVioletRed;
+            Button_livingRoom.BorderRadius = 40;
+            Button_livingRoom.BorderSize = 0;
+            Button_livingRoom.FlatAppearance.BorderSize = 0;
+            Button_livingRoom.FlatStyle = FlatStyle.Flat;
+            Button_livingRoom.Font = new Font("Segoe UI", 19F);
+            Button_livingRoom.ForeColor = Color.White;
+            Button_livingRoom.Location = new Point(1051, 496);
+            Button_livingRoom.Name = "Button_livingRoom";
+            Button_livingRoom.Size = new Size(315, 78);
+            Button_livingRoom.TabIndex = 2;
+            Button_livingRoom.Text = "Aller au salon";
+            Button_livingRoom.TextColor = Color.White;
+            Button_livingRoom.UseVisualStyleBackColor = false;
+            Button_livingRoom.Click += Button_livingRoom_Click;
+            // 
+            // Button_Bathroom
+            // 
+            Button_Bathroom.BackColor = Color.Transparent;
+            Button_Bathroom.BackgroundColor = Color.Transparent;
+            Button_Bathroom.BorderColor = Color.PaleVioletRed;
+            Button_Bathroom.BorderRadius = 40;
+            Button_Bathroom.BorderSize = 0;
+            Button_Bathroom.FlatAppearance.BorderSize = 0;
+            Button_Bathroom.FlatStyle = FlatStyle.Flat;
+            Button_Bathroom.Font = new Font("Segoe UI", 19F);
+            Button_Bathroom.ForeColor = Color.White;
+            Button_Bathroom.Location = new Point(43, 496);
+            Button_Bathroom.Name = "Button_Bathroom";
+            Button_Bathroom.Size = new Size(315, 78);
+            Button_Bathroom.TabIndex = 3;
+            Button_Bathroom.Text = "Aller dans la salle de bain";
+            Button_Bathroom.TextColor = Color.White;
+            Button_Bathroom.UseVisualStyleBackColor = false;
+            Button_Bathroom.Click += Button_Bathroom_Click;
             // 
             // Frame_hall
             // 
@@ -37,11 +79,16 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
             BackgroundImageLayout = ImageLayout.Stretch;
+            Controls.Add(Button_Bathroom);
+            Controls.Add(Button_livingRoom);
             Name = "Frame_hall";
             Size = new Size(1910, 1100);
             ResumeLayout(false);
         }
 
         #endregion
+
+        public WindowsFormsApp1.RJButton Button_livingRoom;
+        public WindowsFormsApp1.RJButton Button_Bathroom;
     }
 }
--- Undertale/Frame_hall.cs
+++ Undertale/Frame_hall.cs
@@ -12,9 +12,166 @@ namespace Undertale
 {
     public partial class Frame_hall : UserControl
     {
+        public Frame_LivingRoom Usercontrol6;
+        public Frame_Bathroom Usercontrol7;
+
+
         public Frame_hall()
         {
             InitializeComponent();
+            Usercontrol6 = new Frame_LivingRoom();
+            Usercontrol6.Visible = false;
+            Controls.Add(Usercontrol6);
+
+            Usercontrol7 = new Frame_Bathroom();
+            Usercontrol7.Visible = false;
+            Controls.Add(Usercontrol7);
+        }
+
+        private void Button_livingRoom_Click(object sender, EventArgs e)
+        {
+            Game parentForm = this.FindForm() as Game;
+            if (parentForm != null)
+            {
+                parentForm.Invoke(new Action(() =>
+                {
+
+                    Usercontrol6.Visible = true;
+                    parentForm.EnterName_pictureBox.Visible = false;
+                    parentForm.TextBox_EnterName.Visible = false;
+                    Button_livingRoom.Visible = false;
+                    Button_Bathroom.Visible = false;
+
+                }));
+            }
+
+            Choice_Dungeon secondForm = new Choice_Dungeon();
+            if (secondForm != null)
+            {
+                secondForm.ButtonEnterDungeon.Visible = false;
+                secondForm.Button_exit_dungeon.Visible = false;
+            }
+
+            Frame_TalkToGoatInDungeon thirdForm = new Frame_TalkToGoatInDungeon();
+            if (thirdForm != null)
+            {
+                thirdForm.Button_ContinueTalkToGoat.Visible = false;
+                thirdForm.Button_FollowTheGoat_TalkToGoat.Visible = false;
+            }
+
+            Frame_inFrontOfHouse fourthForm = new Frame_inFrontOfHouse();
+
+            if (fourthForm != null)
+            {
+                if (fourthForm.InvokeRequired)
+                {
+                    fourthForm.Invoke(new Action(() =>
+                    {
+                        Usercontrol6.Visible = true;
+                        fourthForm.userControl4.Visible = false;
+                        fourthForm.ButtonGoToDoor.Visible = false;
+
+                    }));
+                }
+            }
+            Frame_InFrontOfTheDoor fifthForm = new Frame_InFrontOfTheDoor();
+
+            if (fifthForm != null)
+            {
+                if (fifthForm.InvokeRequired)
+                {
+                    fifthForm.Invoke(new Action(() =>
+                    {
+                        Usercontrol6.Visible = true;
+                        fifthForm.Usercontrol5.Visible = false;
+                        fifthForm.ButtonEnter_InFrontOfTheDoor.Visible = false;
+
+                    }));
+                }
+            }
+        }
+
+
+
+
+        private void Button_Bathroom_Click(object sender, EventArgs e)
+        {
+            Game parentForm = this.FindForm() as Game;
+            if (parentForm != null)
+            {
+                parentForm.Invoke(new Action(() =>
+                {
+
+                    Usercontrol7.Visible = true;
+                    parentForm.EnterName_pictureBox.Visible = false;
+                    parentForm.TextBox_EnterName.Visible = false;
+                    Button_livingRoom.Visible = false;
+                    Button_Bathroom.Visible = false;
+
+                }));
+            }
+
+            Choice_Dungeon secondForm = new Choice_Dungeon();
+            if (secondForm != null)
+            {
+                secondForm.ButtonEnterDungeon.Visible = false;
+                secondForm.Button_exit_dungeon.Visible = false;
+            }
+
+            Frame_TalkToGoatInDungeon thirdForm = new Frame_TalkToGoatInDungeon();
+            if (thirdForm != null)
+            {
+                thirdForm.Button_ContinueTalkToGoat.Visible = false;
+                thirdForm.Button_FollowTheGoat_TalkToGoat.Visible = false;
+            }
+
+            Frame_inFrontOfHouse fourthForm = new Frame_inFrontOfHouse();
+
+            if (fourthForm != null)
+            {
+                if (fourthForm.InvokeRequired)
+                {
+                    fourthForm.Invoke(new Action(() =>
+                    {
+                        Usercontrol6.Visible = true;
+                        fourthForm.userControl4.Visible = false;
+                        fourthForm.ButtonGoToDoor.Visible = false;
+
+                    }));
+                }
+            }
+            Frame_InFrontOfTheDoor fifthForm = new Frame_InFrontOfTheDoor();
+
+            if (fifthForm != null)
+            {
+                if (fifthForm.InvokeRequired)
+                {
+                    fifthForm.Invoke(new Action(() =>
+                    {
+                        Usercontrol6.Visible = true;
+                        fifthForm.Usercontrol5.Visible = false;
+                        fifthForm.ButtonEnter_InFrontOfTheDoor.Visible = false;
+
+                    }));
+                }
+            }
+            Frame_InFrontOfTheDoor sixthForm = new Frame_InFrontOfTheDoor();
+
+            if (fifthForm != null)
+            {
+                if (fifthForm.InvokeRequired)
+                {
+                    fifthForm.Invoke(new Action(() =>
+                    {
+                        Usercontrol7.Visible = true;
+                        fifthForm.Usercontrol5.Visible = false;
+                        fifthForm.ButtonEnter_InFrontOfTheDoor.Visible = false;
+
+                    }));
+                }
+            }
         }
     }
 }
+
+
