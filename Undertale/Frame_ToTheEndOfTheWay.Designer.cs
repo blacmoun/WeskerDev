0ad0bafe385bb1926009cef0151651ba0eef6c5b
Marc-Antoine Aboud <pl23idy@eduvaud.ch>
2023-12-19 12:07:22 +0100

    Feat: new frame (Entrence Market)

--- Undertale/Frame_ToTheEndOfTheWay.Designer.cs
+++ Undertale/Frame_ToTheEndOfTheWay.Designer.cs
@@ -29,7 +29,28 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frame_ToTheEndOfTheWay));
+            Button_EnterInTheMarket = new WindowsFormsApp1.RJButton();
             SuspendLayout();
+            // 
+            // Button_EnterInTheMarket
+            // 
+            Button_EnterInTheMarket.BackColor = Color.Transparent;
+            Button_EnterInTheMarket.BackgroundColor = Color.Transparent;
+            Button_EnterInTheMarket.BorderColor = Color.PaleVioletRed;
+            Button_EnterInTheMarket.BorderRadius = 40;
+            Button_EnterInTheMarket.BorderSize = 0;
+            Button_EnterInTheMarket.FlatAppearance.BorderSize = 0;
+            Button_EnterInTheMarket.FlatStyle = FlatStyle.Flat;
+            Button_EnterInTheMarket.Font = new Font("Segoe UI", 19F);
+            Button_EnterInTheMarket.ForeColor = Color.White;
+            Button_EnterInTheMarket.Location = new Point(551, 638);
+            Button_EnterInTheMarket.Name = "Button_EnterInTheMarket";
+            Button_EnterInTheMarket.Size = new Size(315, 82);
+            Button_EnterInTheMarket.TabIndex = 2;
+            Button_EnterInTheMarket.Text = "Entrer";
+            Button_EnterInTheMarket.TextColor = Color.White;
+            Button_EnterInTheMarket.UseVisualStyleBackColor = false;
+            Button_EnterInTheMarket.Click += Button_EnterInTheMarket_Click;
             // 
             // Frame_ToTheEndOfTheWay
             // 
@@ -37,11 +58,14 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
             BackgroundImageLayout = ImageLayout.Stretch;
+            Controls.Add(Button_EnterInTheMarket);
             Name = "Frame_ToTheEndOfTheWay";
             Size = new Size(1910, 1100);
             ResumeLayout(false);
         }
 
         #endregion
+
+        public WindowsFormsApp1.RJButton Button_EnterInTheMarket;
     }
 }
--- Undertale/Frame_ToTheEndOfTheWay.cs
+++ Undertale/Frame_ToTheEndOfTheWay.cs
@@ -12,9 +12,41 @@ namespace Undertale
 {
     public partial class Frame_ToTheEndOfTheWay : UserControl
     {
+
+        public Frame_EntrenceMarket usercontrol13;
         public Frame_ToTheEndOfTheWay()
         {
             InitializeComponent();
+
+            usercontrol13 = new Frame_EntrenceMarket();
+            usercontrol13.Visible = false;
+            Controls.Add(usercontrol13);
+        }
+
+        private void Button_EnterInTheMarket_Click(object sender, EventArgs e)
+        {
+            Game parentForm = this.FindForm() as Game;
+            if (parentForm != null)
+            {
+                parentForm.Invoke(new Action(() =>
+                {
+
+                    parentForm.EnterName_pictureBox.Visible = false;
+                    parentForm.TextBox_EnterName.Visible = false;
+
+
+                }));
+            }
+
+            Choice_Dungeon secondForm = new Choice_Dungeon();
+            if (secondForm != null)
+            {
+                secondForm.ButtonEnterDungeon.Visible = false;
+                secondForm.Button_exit_dungeon.Visible = false;
+                Button_EnterInTheMarket.Visible = false;
+                usercontrol13.Visible=true;
+            }
+
         }
     }
 }
